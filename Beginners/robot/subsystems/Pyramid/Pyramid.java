95faf7d8d0c3690c4ee9c744b1c70580a2cf470b
Michelle <158527456+Stellay2976@users.noreply.github.com>
2025-05-16 01:38:01 -0400

    Lessons for the rookies

---
+++ Beginners/robot/subsystems/Pyramid/Pyramid.java
@@ -0,0 +1,7 @@
+package Beginners.robot.subsystems.Pyramid;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        System.out.println("It is easy to calculate the volume of a Pyramid.");
+    }
+}
